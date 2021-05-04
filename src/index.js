71337c320232e60f832cc383cf83e01ff992e622
Carlos Alberto <carloshtcursos@gmail.com>
2021-05-03 23:50:23 -0400

    use port from env

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const passportLib = require('passport');
 const { buildOAuthApi } = require('./auth');
-
 const { app, passport }  = buildOAuthApi(express(), passportLib);
-
+const { PORT = 3000 } = process.env 
 app.use(passport.initialize());
 
-app.listen(3000)
+app.listen(+PORT)
