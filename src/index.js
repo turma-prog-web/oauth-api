506628f6a8cf0428cae0c30f00f3dcba82eb82d6
Carlos Alberto <carloshtcursos@gmail.com>
2021-06-27 11:14:03 -0400

    task(): add callback on start aplication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('./config').initializeEnvironmentVariables('.env');
 const express = require('express');
 const passportLib = require('passport');
+const logger = require('./helpers/logger');
 const { buildOAuthApi } = require('./auth');
-const { app, passport }  = buildOAuthApi(express(), passportLib);
-const { PORT = 3000 } = process.env
+const { app, passport } = buildOAuthApi(express(), passportLib);
+const { PORT } = process.env;
 
-
-app.listen(+PORT)
+app.listen(+PORT, () => {
+  logger.info(`oauth microservice running at the port ${PORT}`);
+});
