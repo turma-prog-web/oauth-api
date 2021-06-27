8052919a7cdbe6c698d60f360824fc39cceb7b12
Carlos Alberto <carloshtcursos@gmail.com>
2021-06-27 11:15:03 -0400

    task(): add redirect to main api

--- src/auth/controllers/index.js
+++ src/auth/controllers/index.js
@@ -1,23 +1,29 @@
-const customError = require('../../helpers/customError')
-const { SUCCESS } = require('../../helpers/http-status-code')
+const querystring = require('querystring');
+const logger = require('../../helpers/logger');
+const customError = require('../../helpers/customError');
+const todoApiUrl = process.env.TODO_API_URL;
+
+const getUrlToRedirect = (key, payload) => {
+  const query = querystring.stringify({ [key]: JSON.stringify(payload) });
+  return `${todoApiUrl}?${query}`;
+};
 
 const authenticate = (req, res) => {
-    if(!req.user){
-        const { preconditionFiled } = customError;
-        return res.status(preconditionFiled.statusCode).send(preconditionFiled)
-    }
-    const user = handlerUserInformations(req.user);
-    return res.status(SUCCESS).send({ user });
-}
-
+  if (!req.user) {
+    const { preconditionFiled } = customError;
+    logger.error(preconditionFiled);
+    return res.redirect(getUrlToRedirect('error', preconditionFiled));
+  }
+  const user = handlerUserInformations(req.user);
+  return res.redirect(getUrlToRedirect('user', user));
+};
 
 const handlerUserInformations = (user) => ({
-    name: user.provider == "github" ? user.username : user.displayName,
-    code: user.id,
-    email: user?.emails[0]?.value || '',
-    picture: user.photos[0]?.value,
-    active: true
-})
+  name: user.provider == 'github' ? user.username : user.displayName,
+  code: user.id,
+  email: user?.emails[0]?.value || '',
+  picture: user.photos[0]?.value,
+  active: true,
+});
 
-
-module.exports = { authenticate }
+module.exports = { authenticate };
